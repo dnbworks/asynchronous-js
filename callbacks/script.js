249f609663d093a0a9501d9225e4f421ff59ca2a
Nsaako Dooyong <dooyongnsaakobalderama@gmail.com>
2021-02-08 10:31:34 +0700

    callbacks

--- callbacks/script.js
+++ callbacks/script.js
@@ -20,19 +20,35 @@ const getTodos = (resource, callback) => {
 }
 
 
-getTodos("followers.json", (err, data) => {
+getTodos("https://api.github.com/users/PilgrimMemoirs/following", (err, data) => {
     console.log("callback is fired");
     if(err){
         console.log(err);
     } else {
         console.log(data);
-        getTodos("https://api.github.com/users/dnbworks/followers", (err, data) => {
-            console.log("callback is fired");
-            if(err){
-                console.log(err);
-            } else {
-                console.log(data);
-            }
-        });
+        insertToDom(data);
+
+        // getTodos("https://api.github.com/users/dnbworks/followers", (err, data) => {
+        //     console.log("callback is fired");
+        //     if(err){
+        //         console.log(err);
+        //     } else {
+        //         console.log(data);
+        //     }
+        // });
     }
 });
+
+function insertToDom(data){
+    var wrapper = document.querySelector(".wrapper");
+    var person = '<div class="individual"><img src="" alt="" srcset=""><h2></h2></div>';
+    for (let index = 0; index < data.length; index++) {
+        const element = data[index];
+
+        person += '<div class="individual"><img src="' + element.avatar_url + '" alt="" srcset=""><h2>' + element.html_url + '</h2></div>';
+
+        wrapper.insertAdjacentHTML("afterbegin", person);
+        
+    }
+
+}
